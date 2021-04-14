3a4a88aae59ae8e8c6f1c428b878e6e43273490f
Corey Colligan <coreyleec91288@gmail.com>
2021-04-14 14:18:22 -0400

    p, h2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-let addToy = false;
-
-const toyName = document.createElement("h2")
+// let addToy = false;
   // h2.innerText.toy.name
-  const toyLikes = document.createElement("p")
-  
-  const submit = document.createElement("button")
-  submit.className = "like-btn"
-  submit.id = "[toy_id]"
-  const toyForm = document.createElement("form")
   
   const toyCollection = document.querySelector("#toy-collection")
   
-  // console.log(toyCollection)
   
   // const imgToDiv = () => {
     fetch("http://localhost:3000/toys")
@@ -20,15 +11,40 @@ const toyName = document.createElement("h2")
     .then((toyArray) => {
       toyArray.forEach(function(toyObj){
       let toyCard = document.createElement ("div")
+        toyCard.className = "card" 
+      let toyName = document.createElement("h2")
       let toyImage = document.createElement("img")
         toyImage.className = "toy-avatar"
-        toyCard.className = "card" 
-        toyCard.src = toyObj.image 
-      toyCard.append(toyImage) 
-      toyCollection.append(toyCard)
+      let likes = document.createElement("p")
+        toyCard.append(toyName)
+          toyName.innerText = toyObj.name
+        toyCard.append(toyImage) 
+          toyImage.src = toyObj.image 
+        toyCard.append(likes)
+          likes.innerText = toyObj.likes
+        toyCollection.append(toyCard)
+        
+        
+
+        // let submit = document.createElement("button")
+        // submit.className = "like-btn"
+        // submit.id = "[toy_id]"
+        
+
+
     });
   });
-// }
+
+  // fetch("http://localhost:3000/toys")
+  // .then(res => res.json())
+  // .then((toyArray) => {
+  //   toyArray.forEach(function(toyObj){      
+  //     });
+  //   });
+
+    // let formPlace = document.querySelector("#toy-header")
+    
+  // }
 // console.log(imgToDiv)
 
 //  fetch ("http://localhost:3000/toys"), 
@@ -39,15 +55,19 @@ document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
-    
-    
     // hide & seek with the form
+    console.log("hello")
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
     } else {
       toyFormContainer.style.display = "none";
     }
+        
+        const toyForm = document.querySelector(".add-toy-form")
+        toyFormContainer.append(toyForm)
+
+
   });
 });
 
