7afd75a5edea59029dc8561cb8c17bda48778ed2
Brosner522 <rosnerbb@gmail.com>
2021-04-14 09:43:23 -0400

    Second test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 let addToy = false;
 // Added code to test
+// Second line added
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
